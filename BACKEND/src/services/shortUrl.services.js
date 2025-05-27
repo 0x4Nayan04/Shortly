857ab160e1e971798db92b35a531be4c6510920d
Nayan Swarnkar <nayan_s@ce.iitr.ac.in>
2025-05-26 22:00:37 -0700

    refactor: improve short URL generation logic and clean up commented code

--- BACKEND/src/services/shortUrl.services.js
+++ BACKEND/src/services/shortUrl.services.js
@@ -3,9 +3,17 @@ import short_urlModel from "../schema/shortUrl.model.js";
 import { generateNanoId } from "../utlis/helper.js";
 
 const short_url = generateNanoId(7);
+
 if (!short_url) {
   throw new Error("Failed to generate short URL");
 }
+
+/* const existingShortUrl = await short_urlModel.findOne({ short_url });
+
+if (existingShortUrl) {
+  return existingShortUrl.short_url;
+} */
+
 export const createShortUrlWithoutUser = async (full_url) => {
   await saveShortUrl(short_url, full_url);
 
