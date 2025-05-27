8a402203d1bb951a35a8b50297ea624de1042c63
Nayan Swarnkar <nayan_s@ce.iitr.ac.in>
2025-05-27 05:57:21 -0700

    TanStack Query Setup Done

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -1,6 +1,11 @@
-import { StrictMode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+const queryClient = new QueryClient();
+createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
